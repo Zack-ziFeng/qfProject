ed63fc0d87c88d1d63049f56a016900beb573f27
ZZF <1282229539@qq.com>
2018-08-22 21:04:16 +0800

    use require

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
-jQuery(function($) {
+require.config({
+	paths: {
+		'jquery':'../lib/jquery-3.3.1'
+	}
+});
+
+require(['jquery'], function($){
+	//加载头部和尾部，侧边栏
 	$('#header').load('../html/commonHTML.html header');
 	$('#nav').load('../html/commonHTML.html nav');
 	$('#aside').load('../html/commonHTML.html .aside');
 	$('#footer').load('../html/commonHTML.html footer');
 	$('.nav_list').css('display', 'block');
+
+	function bannerImg () {}
 });
